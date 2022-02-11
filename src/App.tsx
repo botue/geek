fe506ed800a669ef9cb6d829f865950da99f832e
botue <botue@sina.com>
2022-02-11 23:30:43 +0800

    配置 basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { Router, Route, Redirect, Switch } from 'react-router-dom'
+import { Router, Route, Redirect, Switch } from 'react-router-dom';
 
-import { history } from '@/utils'
-import AuthRoute from '@/components/AuthRoute'
-import { KeepAlive } from './components/KeepAlive'
+import { history } from '@/utils';
+import AuthRoute from '@/components/AuthRoute';
+import { KeepAlive } from './components/KeepAlive';
 
-import Login from './pages/Login'
-import Layout from './pages/Layout'
-import ProfileEdit from './pages/Profile/Edit'
-import Search from './pages/Search'
-import SearchResult from './pages/Search/Result'
-import Article from './pages/Article'
-import ProfileChat from './pages/Profile/Chat'
-import Feedback from './pages/Profile/Feedback'
-import UserActivity from './pages/Profile/Activity'
-import UserFans from './pages/Profile/Fans'
-import UserArticles from './pages/Profile/Articles'
-import UserNotification from './pages/Profile/Notification'
-import NotFound from './pages/NotFound'
+import Login from './pages/Login';
+import Layout from './pages/Layout';
+import ProfileEdit from './pages/Profile/Edit';
+import Search from './pages/Search';
+import SearchResult from './pages/Search/Result';
+import Article from './pages/Article';
+import ProfileChat from './pages/Profile/Chat';
+import Feedback from './pages/Profile/Feedback';
+import UserActivity from './pages/Profile/Activity';
+import UserFans from './pages/Profile/Fans';
+import UserArticles from './pages/Profile/Articles';
+import UserNotification from './pages/Profile/Notification';
+import NotFound from './pages/NotFound';
 
 // const UserArticleList = lazy(() => import('@/pages/Profile/Article'))
 // const UserFans = lazy(() => import('@/pages/Profile/Fans'))
@@ -74,21 +74,21 @@ const App = () => {
           {/* path="*" 表示匹配所有路由 */}
           <Route
             path="*"
-            render={props => {
+            render={(props) => {
               // 注意：此处需要将 /home 排除掉，因为 Home 对应的路由没有放在 Switch 中
               //      所以，路由匹配时，会全部匹配一次，当匹配到 Switch 内部时，此处，就会匹配成功
               //      而我们不希望此处的 * 来匹配 Home
               //      因此，此处将 /home 排除掉
               if (props.location.pathname.startsWith('/home')) {
-                return null
+                return null;
               }
-              return <NotFound />
+              return <NotFound />;
             }}
           ></Route>
         </Switch>
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/utils/history.ts
+++ src/utils/history.ts
@@ -1,8 +1,10 @@
 // 1 导入 createBrowserHistory 函数
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory } from 'history';
 
 // 2 创建 history 对象
-const history = createBrowserHistory()
+const history = createBrowserHistory({
+  basename: '/geek',
+});
 
 // 3 导出创建好的 history
-export { history }
+export { history };
